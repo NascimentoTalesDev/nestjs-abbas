4ac039e40f77a7e17c306278da146a04b7c90d73
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-08-27 11:59:55 -0400

    test: add UserDataBuild

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,16 +1,12 @@
+import { UserDataBuilder } from "../../../testing/user-data-builder"
 import { UserEntity, UserProps } from "../../user.entity"
-import { faker } from "@faker-js/faker"
 
 describe("UserEntity unit tests",  () => {
   let props: UserProps
   let sut: UserEntity
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
+    props = UserDataBuilder({})
     sut = new UserEntity(props)
   })
 
