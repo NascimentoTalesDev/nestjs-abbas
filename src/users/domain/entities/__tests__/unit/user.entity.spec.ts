ccf4c4fefcfa2ad4d2989fc11ddc40fb357b8070
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-08-27 22:06:00 -0400

    test: update methods

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -22,18 +22,44 @@ describe("UserEntity unit tests",  () => {
     expect(sut.props.name).toEqual(props.name)
     expect(typeof sut.props.name).toBe("string")
   });
+
+  it('Setter of name field', () => {
+    sut['name'] = 'Other Name'
+    expect(sut.props.name).toEqual('Other Name')
+    expect(typeof sut.props.name).toBe("string")
+  });
+
   it('Getter of email field', () => {
     expect(sut.props.email).toBeDefined()
     expect(sut.props.email).toEqual(props.email)
     expect(typeof sut.props.email).toBe("string")
   });
+
   it('Getter of password field', () => {
     expect(sut.props.password).toBeDefined()
     expect(sut.props.password).toEqual(props.password)
     expect(typeof sut.props.password).toBe("string")
   });
+
+  it('Setter of password field', () => {
+    sut['password'] = "Other Password"
+    expect(sut.props.password).toEqual("Other Password")
+    expect(typeof sut.props.password).toBe("string")
+  });
+
   it('Getter of createdAt field', () => {
     expect(sut.props.createdAt).toBeDefined()
     expect(sut.props.createdAt).toBeInstanceOf(Date)
   });
+
+  it('Should update a user', () => {
+    sut.update('Other Name')
+    expect(sut.props.name).toEqual('Other Name')
+  });
+
+  it('Should update a user password', () => {
+    sut.updatePassword('Other Password')
+    expect(sut.props.password).toEqual('Other Password')
+  });
+
 })
